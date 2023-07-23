3a7852bc6b53e58e9be5576bca8ec019c73c94e8
AnoPiS <omer.mkawy.om@gmail.com>
2023-07-23 05:38:53 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:messenger_rep/modules/login_screen.dart';
 
+import 'shared/bloc_0bsever.dart';
+
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
 
